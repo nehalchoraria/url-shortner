5c56302613f48c35f9d6a4052106dcbf1412f006
unknown <nehalchoraria09@gmail.com>
2021-02-21 12:10:44 +0530

    deployment ready

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -25,9 +25,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['links-shortners.herokuapp.com']
 
 
 # Application definition
